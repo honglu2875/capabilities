6e48acf5d8d421f4b21925ecc8c5f972fb8e691a
Ed Ayers <contact@edayers.com>
2023-05-09 21:35:34 +0100

    test: include tests in docs

--- tests/test_dec.py
+++ tests/test_dec.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from capabilities import llm
 
 
@@ -17,5 +18,66 @@ def test_llm():
         print(result)
 
 
+def test_docs_demo():
+    from pydantic import BaseModel
+    from capabilities import Capability
+
+    # define the input and output models
+    class InputText(BaseModel):
+        text: str
+
+    class WordTranslation(BaseModel):
+        source: str
+        target: str
+
+    class TranslationOutput(BaseModel):
+        translation: str
+        word_translations: list[WordTranslation]
+
+    # create an input instance
+    inp = InputText(
+        text="Understand: I'll slip quietly away through twilit meadows with only this one dream - you come too."
+    )
+
+    # provide some instructions
+    instructions = "Given the input `text`, produce a `french_translation` which translates the `text` into French. Also produce a word-level translation called `word_translations`, which is a list of (english word, french transliteration) pairs."
+
+    # print the task to console
+    c = Capability("blazon/structured")
+    translation_output: TranslationOutput = c(
+        InputText, TranslationOutput, instructions, inp
+    )
+    print(translation_output)
+
+
+def test_docs_demo2():
+    from pydantic import BaseModel
+    from capabilities import Capability
+
+    class WordTranslation(BaseModel):
+        source: str
+        target: str
+
+    class TranslationOutput(BaseModel):
+        translation: str
+        word_translations: list[WordTranslation]
+
+    @llm
+    def translate(input_text: str) -> TranslationOutput:
+        """Given the input `text`, produces a `french_translation`
+        which translates the `text` into French.
+        Also produce a word-level translation called `word_translations`,
+        which is a list of (english word, french transliteration) pairs.
+        """
+        ...
+
+    # create an input instance
+    inp = "Understand: I'll slip quietly away through twilit meadows with only this one dream - you come too."
+
+    # print the task to console
+    translation_output = translate(inp)
+    print(translation_output)
+
+
 if __name__ == "__main__":
-    test_llm()
+    test_docs_demo2()
